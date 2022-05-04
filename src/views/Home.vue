34635f655f1109231e119edc8ea15dc2004e2f33
三十年河东 <1322903477@qq.com>
2022-05-04 12:54:57 +0800

    开始初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-form ref="ruleForm" :rules="rules" :model="ruleForm" class="formlogin" label-width="80px">
+      <h1 class="logintitle">登录界面</h1>
+      <el-form-item label="账户" prop="username">
+        <el-input  type="text" auto-complete="false" v-model="ruleForm.username" placeholder="请输入账户 "></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input  type="password" auto-complete="false" v-model="ruleForm.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item label="验证码" prop="code">
+        <el-input  type="text"  auto-complete="false" v-model="ruleForm.code" placeholder="验证码"></el-input>
+        <img src="" >
+      </el-form-item>
+      <el-checkbox v-model="checked" style="margin:20px 0px 20px 260px;">请记住</el-checkbox>
+      <el-button type="primary" style="width:100%" @click="submitForm(ruleForm)">登录</el-button>
+    </el-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      ruleForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      checked: true,
+      rules: {
+        username: [{ required: true, message: '请输入正确账户', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入正确密码', trigger: 'blur' }],
+        code: [{ required: true, message: '请输入验证码', trigger: 'blur' }]
+      }
+
+    }
+  },
+  methods: {
+    // 登录
+    submitForm () {
+      this.$refs.ruleForm.validate((valid) => {
+        if (!valid) return this.$message.error('请输入完整信息')
+        alert('登录成功')
+      })
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
+<style>
+  .formlogin {
+    border-radius: 15px;
+    background-clip: border-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 15px 35px 15px 35px;
+    /* border: 1px solid #ccc; */
+    box-shadow: 0 0 25px #ccc;
+  }
+  .logintitle{
+    margin: 0px 5px 40px auto;
+    text-align: center;
+  }
+  .yanzheng {
+
+  }
+</style>
